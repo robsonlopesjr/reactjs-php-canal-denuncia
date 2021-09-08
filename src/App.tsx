68db95ff343a93b6d95bb91f2629d35da85e70fb
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2021-09-08 13:07:57 -0300

    adicionado componente toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import AppProvider from './hooks';
+
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
@@ -8,7 +10,9 @@ import GlobalStyle from './styles/global';
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Routes />
+      <AppProvider>
+        <Routes />
+      </AppProvider>
       <GlobalStyle />
     </BrowserRouter>
   );
